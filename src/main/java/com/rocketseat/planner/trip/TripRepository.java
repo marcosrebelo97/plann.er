fc20b2af88f78cd79f85a55ca0d98f57303ea82c
Marcos Guerreiro <marcosrebelo9@gmail.com>
2024-07-08 22:38:39 -0400

    1ª aula - Config BD, Criação de Migration, Entidade Trip, Repository, Endpoint de Cadastro e Consulta de Viagem

--- src/main/java/com/rocketseat/planner/trip/TripRepository.java
+++
@@ -1,8 +0,0 @@
-package com.rocketseat.planner.trip;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.UUID;
-
-public interface TripRepository extends JpaRepository<Trip, UUID> {
-}
